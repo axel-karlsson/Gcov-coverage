78b39cf4d0201bd1669fd90a61b7efcb4aa2d30e
Axel Karlsson <axelkarlsson3@gmail.com>
2019-06-18 10:49:09 +0200

    Added functions to parse text and store gcov info in container. Starting to work on functions to handle the data

--- source/app.d
+++ source/app.d
@@ -1,6 +1,71 @@
 import std.stdio;
+import std.string;
+import std.range.primitives : empty;
+
+struct CodeLine
+{
+	string line_nr, execution_nr;
+	auto  toString() const
+	{
+		return format(("Line nr %s: Executed %s times"), line_nr, execution_nr);
+	};
+}
+
+struct Lines{
+	CodeLine[] codelines;
+}
+
+struct CodeLineRange{
+	CodeLine[] codelines;
+
+	this(Lines line){
+		this.codelines = line.codelines;
+	}
+	@property bool empty() const{
+		return codelines.length == 0;
+	}
+	@property ref CodeLine front(){
+		return codelines[0];
+	}
+}
+
+auto split_codeline(ref string line){
+
+	string exenr, linenr;
+	auto splitted = split(line, ":");
+	if(splitted)
+	{
+		exenr = strip(splitted[0]);
+		linenr = strip(splitted[1]);
+	}
+	auto split_line = CodeLine(linenr, exenr);
+	return split_line;
+}
+
+auto count_line_usage(Lines line){
+	//TODO
+
+}
+
+auto addToRange(ref CodeLine line, ref Lines ranges){
+	ranges.codelines ~= line;
+	return ranges;
+
+}
 
 void main()
 {
-	writeln("Hello world");
+	CodeLine line;
+	Lines ranges;
+
+	File file = File("source/test.c.gcov", "r");
+	while(!file.eof()){
+		auto gcov = file.readln();
+		 line = split_codeline(gcov);
+		 ranges = addToRange(line, ranges);
+	}
+	file.close();
+
+	writeln(ranges);
+
 }
